637e65d5f446a72a5d54ca194ecb06c0ff7cc34a
Haley <hscheina.niu@gmail.com>
2018-04-16 18:25:18 -0500

    code cleanup

--- src/AnimationPanel.java
+++ src/AnimationPanel.java
@@ -19,7 +19,6 @@ public class AnimationPanel extends JPanel implements Runnable {
         setLayout(new GridLayout());
         setPreferredSize(new Dimension(300, 500));
         arrayListOfBallObjects = new ArrayList<>();
-        //arrayListOfBallObjects = null;
         dimension = null;
         animationThread = null;
     }
@@ -32,7 +31,6 @@ public class AnimationPanel extends JPanel implements Runnable {
          */
         if (animationThread == null) {
             animationThread = new Thread(this);
-           // animationThread = new Thread(this::run);
             animationThread.start();
         }
 
@@ -51,27 +49,9 @@ public class AnimationPanel extends JPanel implements Runnable {
 
     @Override
     protected void paintComponent(Graphics g) {
-      //  super.paintComponent(g);
-        //Call the superclass version of the method.
         System.out.println("paint component running");
-   //     Dimension size = getSize();
-        //if (size==null) {
-        //    setSize(new Dimension(300,500));
-        //}
-
-        /*
-        • If the Dimension object reference is null, create a set of Ball objects
-            and add them to the ArrayList, then get the dimensions of the panel by
-            calling getSize().
-        • Draw the white background. One easy way to do that is to just a white
-            rectangle the same size as the panel.
-        • Call the move() and draw() methods for each Ball object in the ArrayList.
-            The Dimension object needs to be passed to move(), while the Graphics
-            object needs to be passed to draw().
-         */
         if (dimension == null) {
             dimension = getSize();
-            //Ball ball1 = new Ball(Color.GREEN, 20, (dimension.width * 2 / 3), (dimension.height - 28), -2, -4);
             Ball ball1 = new Ball(Color.GREEN, 20, 0, 0, 0, 0);
             Ball ball2 = new Ball(Color.RED, 20, 10, (dimension.height - 45), -2, -4);
             Ball ball3 = new Ball(Color.BLUE, 20, (dimension.width * 2 / 3), (dimension.height - 45), -2, -4);
@@ -79,11 +59,7 @@ public class AnimationPanel extends JPanel implements Runnable {
             arrayListOfBallObjects.add(ball2);
             arrayListOfBallObjects.add(ball3);
         }
-        //dimension should be w:300 h:500
-        // Dimension d = getSize();
-//            dimension = new Dimension(300,500);
 
-        //TODO: get the coordinates of the rectangle
         g.setColor(Color.WHITE);
         g.fillRect(0, 0, dimension.width, dimension.height);
 
@@ -94,38 +70,7 @@ public class AnimationPanel extends JPanel implements Runnable {
         }
     }
 
- //   @Override
     public void run() {
-        /*
-        This is effectively the “main” method that will run in the separate
-        background thread. It will have a loop that continues while the
-        current thread is equal to the Thread reference data member, e.g.:
-
-                while (Thread.currentThread == animationThread)
-
-        The loop body should put the thread to sleep for a short amount of
-        time (25 milliseconds is about right), and then call repaint()
-        (which will eventually result in paintComponent() being executed).
-        If the thread is interrupted, just return from the method.
-         */
-
-
-        /*
-public static void main(String args[]) throws InterruptedException {
-    SwingUtilities.invokeLater(new Runnable() {
-        public void run() {
-            JFrame frame = new JFrame("Empty JFrame");
-            frame.setSize(new Dimension(1000, 500));
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            PaintComponentTest ilt = new PaintComponentTest();
-            frame.add(ilt);
-            frame.setVisible(true);
-            ilt.setBackground(Color.BLACK);
-        }
-    });
-}
- */
-
         System.out.println("run running");
 
         while (Thread.currentThread() == animationThread) {
@@ -134,7 +79,6 @@ public static void main(String args[]) throws InterruptedException {
             } catch (InterruptedException e) {
                 return;
             }
-            //revalidate();
             repaint();
         }
     }
